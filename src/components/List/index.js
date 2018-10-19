fd9df28df73cac21f5f5f6dff159489271dff703
leofle <leofle@gmail.com>
2018-10-19 23:41:39 +0300

    adding apollo

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,10 +1,23 @@
 import React,{Component, Fragment} from 'react'
 import store from '../../store'
 import ListItem from '../ListItem'
-
+import { compose, graphql } from 'react-apollo'
+import { gql } from 'apollo-boost'
 import { Card, Title, ListClean } from '../../styles'
 
-export default class List extends Component {
+const getRecordsQuery = gql`
+  {
+    records {
+      id
+      name
+      startTime
+      stopTime
+      blobUrl
+    }
+  }
+`;
+
+class List extends Component {
   constructor(props){
     super(props);
     this.state = {
@@ -12,20 +25,33 @@ export default class List extends Component {
     }
   }
   componentDidMount(){
-    this.setState({
-      recordingList: store.getState().records.recordingList
-    })
+    if(!this.props.data.loading){
+      this.setState({
+        recordingList: this.props.data.records
+        // recordingList: store.getState().records.recordingList
+      })
+    }
+  }
+  displayRecords(){
+    let data = this.props.data;
+    if(data.laoding){
+      return(<div>loading...</div>);
+    }
+    else {
+      return data.records && data.records.map((record, index)=> {
+        return <ListItem key={index} items={record}/>;
+      })
+    }
   }
   render() {
     const { recordingList } = this.state;
+    console.log(this.displayRecords())
     return (
       <Fragment>
         <Card>
           <Title>Records list</Title>
           <ListClean>
-            {recordingList && recordingList.map((listItem,index)=>{
-              return <ListItem key={index} items={listItem}/>
-            })}
+            {this.displayRecords()}
           </ListClean>
           <p>{store.getState().labels.label}</p>
         </Card>
@@ -33,3 +59,4 @@ export default class List extends Component {
     )
   }
 }
+export default graphql(getRecordsQuery)(List)
--- src/index.js
+++ src/index.js
@@ -5,16 +5,23 @@ import { ConnectedRouter } from 'connected-react-router'
 import store, { history } from './store'
 import App from './components/App'
 import './index.scss'
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
 
+const client = new ApolloClient({
+  uri:'http://localhost:4000/graphql'
+})
 const target = document.querySelector('#root')
 
 render(
+  <ApolloProvider client={client}>
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <Fragment>
         <App />
       </Fragment>
     </ConnectedRouter>
-  </Provider>,
+  </Provider>
+  </ApolloProvider>,
   target
 )
